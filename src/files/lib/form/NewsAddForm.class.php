3a603f1e4734dfc7c290545f9b1df411cf639044
mutec <webmaster@mysterycode.de>
2016-12-29 05:25:43 +0100

    fix form

--- src/files/lib/form/NewsAddForm.class.php
+++ src/files/lib/form/NewsAddForm.class.php
@@ -81,6 +81,7 @@ class NewsAddForm extends MessageForm {
 	public function readFormParameters() {
 		parent::readFormParameters();
 
+		if (isset($_POST['categoryIDs']) && is_array($_POST['categoryIDs'])) $this->categoryIDs = ArrayUtil::trim($_POST['categoryIDs']);
 		if (isset($_POST['tags']) && is_array($_POST['tags'])) $this->tags = ArrayUtil::trim($_POST['tags']);
 		if (isset($_POST['time']) && !empty($_POST['time'])) $this->time = \DateTime::createFromFormat('Y-m-d\TH:i:sP', $_POST['time'], WCF::getUser()->getTimeZone())->getTimestamp();
 		if (isset($_POST['imageID'])) $this->imageID = intval($_POST['imageID']);
