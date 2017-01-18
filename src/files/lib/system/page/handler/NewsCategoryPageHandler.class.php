9269c3bf75a3e5a792fb967a9c6c9a2a7bd23083
mutec <webmaster@mysterycode.de>
2017-01-18 05:36:31 +0100

    updates news category page handler

--- src/files/lib/system/page/handler/NewsCategoryPageHandler.class.php
+++ src/files/lib/system/page/handler/NewsCategoryPageHandler.class.php
@@ -1,15 +1,16 @@
 <?php
 
 namespace cms\system\page\handler;
+
 use cms\data\category\NewsCategory;
 use wcf\data\category\Category;
-use wcf\system\page\handler\AbstractMenuPageHandler;
+use wcf\system\page\handler\AbstractLookupPageHandler;
 use wcf\system\page\handler\IOnlineLocationPageHandler;
 
 /**
  * {@inheritDoc}
  */
-class NewsCategoryPageHandler extends AbstractMenuPageHandler implements IOnlineLocationPageHandler {
+class NewsCategoryPageHandler extends AbstractLookupPageHandler implements IOnlineLocationPageHandler {
 	use TNewsOnlineLocationPageHandler;
 
 	/**
@@ -41,5 +42,57 @@ class NewsCategoryPageHandler extends AbstractMenuPageHandler implements IOnline
 			return false;
 
 		return $category->isAccessible();
+	}
+
+	/**
+	 * @inheritdoc
+	 */
+	public function lookup($searchString) {
+		$sql = "(
+			SELECT  categoryID, title, title as name
+			FROM    wcf" . WCF_N . "_category
+			WHERE   title = ?
+		)
+		UNION
+		(
+			SELECT  0 as categoryID, languageItemValue as title, languageItem as name
+			FROM    wcf" . WCF_N . "_language_item
+			WHERE   languageID = ?
+					AND languageItem LIKE ?
+					AND languageItemValue LIKE ?
+		)";
+		$statement = WCF::getDB()->prepareStatement($sql, 10);
+		$statement->execute([
+			'%' . $searchString . '%',
+			WCF::getLanguage()->languageID,
+			'wcf.category.title%',
+			'%' . $searchString . '%'
+		]);
+
+		$results = [];
+		while ($row = $statement->fetchArray()) {
+			$categoryID = $row['categoryID'];
+			if ($row['categoryID']) {
+				if ($row['title'] === 'wcf.category.title' . $categoryID)
+					continue;
+			} else {
+				if (preg_match('~^wcf\.category\.title(\d+)$~', $row['title'], $matches))
+					$categoryID = $matches[1];
+				else
+					continue;
+			}
+
+			$category = new NewsCategory(new Category($categoryID));
+
+			$results[] = [
+				'description' => $category->description,
+				'image' => 'fa-text-o',
+				'link' => $category->getLink(),
+				'objectID' => $categoryID,
+				'title' => $row['title']
+			];
+		}
+
+		return $results;
 	}
 }
