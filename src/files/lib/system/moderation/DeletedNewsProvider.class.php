59f40a1c50e8030d6b3d9c36724bd7cb1bb16ec3
mutec <webmaster@mysterycode.de>
2017-02-01 18:51:39 +0100

    feature: moderation (report, activation, deletedContent)
    (cherry picked from commit 22ed0e4)

---
+++ src/files/lib/system/moderation/DeletedNewsProvider.class.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace cms\system\moderation;
+use cms\data\news\DeletedNewsList;
+use wcf\system\moderation\IDeletedContentProvider;
+
+/**
+ * @author	Florian Gail
+ * @copyright	2013-2017 codeQuake
+ * @license	GNU Lesser General Public License <http://opensource.org/licenses/lgpl-license.php>
+ */
+class DeletedNewsProvider implements IDeletedContentProvider {
+	/**
+	 * @inheritDoc
+	 */
+	public function getObjectList() {
+		return new DeletedNewsList();
+	}
+
+	/**
+	 * @inheritDoc
+	 */
+	public function getTemplateName() {
+		return 'deletedNewsList';
+	}
+
+	/**
+	 * @inheritDoc
+	 */
+	public function getApplication() {
+		return 'cms';
+	}
+}
