4233645cf4f7d4b6f64527535ca8d15f88e8e078
mutec <webmaster@mysterycode.de>
2017-03-13 13:57:29 +0100

    fixed tagging templatename

--- src/files/lib/system/tagging/TaggableNews.class.php
+++ src/files/lib/system/tagging/TaggableNews.class.php
@@ -26,7 +26,7 @@ class TaggableNews implements ITaggable {
 	 * @inheritDoc
 	 */
 	public function getTemplateName() {
-		return 'newsListItems     Message';
+		return 'newsListItemsMessage';
 	}
 
 	/**
