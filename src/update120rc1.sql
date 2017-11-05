a8e3a12a910db92078e963b9974139680be30037
mutec <webmaster@mysterycode.de>
2017-11-05 17:44:10 +0100

    prepare updates

--- src/update120rc1.sql
+++
@@ -1,4 +0,0 @@
--- changes WCF 2.0 > WSC 3.0
-ALTER TABLE cms1_news DROP COLUMN enableSmilies;
-ALTER TABLE cms1_news DROP COLUMN enableBBCodes;
-ALTER TABLE cms1_news ADD COLUMN hasEmbeddedObjects TINYINT(1)   NOT NULL DEFAULT 0;
