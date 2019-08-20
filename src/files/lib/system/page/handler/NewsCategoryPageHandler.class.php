fa8b9bf68c307c5b6e2c30ad72629d5f05d0e9ef
mutec <mysterycode@mysterycode.de>
2019-08-20 15:19:11 +0200

    fix news category online location

--- src/files/lib/system/page/handler/NewsCategoryPageHandler.class.php
+++ src/files/lib/system/page/handler/NewsCategoryPageHandler.class.php
@@ -4,6 +4,8 @@ namespace cms\system\page\handler;
 
 use cms\data\category\NewsCategory;
 use wcf\data\category\Category;
+use wcf\data\page\Page;
+use wcf\data\user\online\UserOnline;
 use wcf\system\page\handler\AbstractLookupPageHandler;
 use wcf\system\page\handler\IOnlineLocationPageHandler;
 use wcf\system\WCF;
@@ -95,5 +97,19 @@ class NewsCategoryPageHandler extends AbstractLookupPageHandler implements IOnli
 		}
 
 		return $results;
+	}
+
+	/**
+	 * @inheritdoc
+	 */
+	public function getOnlineLocation(Page $page, UserOnline $user) {
+		if ($user->pageObjectID === null)
+			return '';
+
+		$category = new NewsCategory(new Category($user->pageObjectID));
+		if ($category === null || !$category->isAccessible())
+			return '';
+
+		return WCF::getLanguage()->getDynamicVariable('wcf.page.onlineLocation.' . $page->identifier, ['category' => $category]);
 	}
 }
